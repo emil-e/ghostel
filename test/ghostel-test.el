e39d30a7e7d4ee658fab444129d932b7ad2522ae
Emil Sahlén <shadewind@gmail.com>
2026-04-26 09:55:13 +0200

    Make ghostel-test-compile-multiline-end-to-end run when shell is not /bin/sh

--- test/ghostel-test.el
+++ test/ghostel-test.el
@@ -3455,6 +3455,7 @@ spawns `sh -c COMMAND' directly, so the shell parses the paragraph
 normally."
   (skip-unless (file-executable-p "/bin/sh"))
   (let* ((buf-name "*ghostel-test-multiline-compile*")
+		 (shell-file-name "/bin/sh")
          (script "for i in 1 2 3; do\n  echo line-$i\ndone\nexit 7")
          (inhibit-message t)
          (save-some-buffers-default-predicate (lambda () nil)))
